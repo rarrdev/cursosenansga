0fdd6a3ff49f5a295b23c5ad257305d77301b660
rariverasuper <67770958+rariverar@users.noreply.github.com>
2022-10-04 17:50:45 -0500

    agregar cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import docentesRoutes from './routes/docente.routes.js'
-import cors from "cors"
+
+const cors = require('cors')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
 
 app.use('/api', docentesRoutes)
 
